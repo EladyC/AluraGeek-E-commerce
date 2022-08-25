a1549edff6cd68dfcb92eb07cd6fba5f7bc14998
Lady Espinoza <87677424+EladyC@users.noreply.github.com>
2022-08-25 13:02:24 -0500

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,188 @@
+import star_1 from "./img/star_1.png";
+import star_2 from "./img/star_2.png";
+import star_3 from "./img/star_3.png";
+import star_4 from "./img/star_4.png";
+import star_5 from "./img/star_5.png";
+import star_6 from "./img/star_6.png";
+
+import consolas_1 from "./img/consolas_1.png";
+import consolas_2 from "./img/consolas_2.png";
+import consolas_3 from "./img/consolas_3.png";
+import consolas_4 from "./img/consolas_4.png";
+import consolas_5 from "./img/consolas_5.png";
+import consolas_6 from "./img/consolas_6.png";
+
+import diversos_1 from "./img/diversos_1.png";
+import diversos_2 from "./img/diversos_2.png";
+import diversos_3 from "./img/diversos_3.png";
+import diversos_4 from "./img/diversos_4.png";
+import diversos_5 from "./img/diversos_5.png";
+import diversos_6 from "./img/diversos_6.png";
+
+export const categories = [
+  {
+    nombre: "Star Wars",
+    images: [
+      {
+        img: star_1,
+        nombre: "Producto XYZ",
+      },
+      {
+        img: star_2,
+        nombre: "Producto XYZ",
+      },
+      {
+        img: star_3,
+        nombre: "Producto XYZ",
+      },
+      {
+        img: star_4,
+        nombre: "Producto XYZ",
+      },
+      {
+        img: star_5,
+        nombre: "Producto XYZ",
+      },
+      {
+        img: star_6,
+        nombre: "Producto XYZ",
+      },
+    ],
+  },
+  {
+    nombre: "Consolas",
+    images: [
+      {
+        img: consolas_1,
+        nombre: "Control XYZ",
+      },
+      {
+        img: consolas_2,
+        nombre: "Control y consola XYZ",
+      },
+      {
+        img: consolas_3,
+        nombre: "Consola XYZ",
+      },
+      {
+        img: consolas_4,
+        nombre: "Control XYZ",
+      },
+      {
+        img: consolas_5,
+        nombre: "Consola XYZ",
+      },
+      {
+        img: consolas_6,
+        nombre: "Game Boy Color",
+      },
+    ],
+  },
+  {
+    nombre: "Diversos",
+    images: [
+      {
+        img: diversos_1,
+        nombre: "Camisa Atari",
+      },
+      {
+        img: diversos_2,
+        nombre: "Camisa SNES",
+      },
+      {
+        img: diversos_3,
+        nombre: "Control y consola XYZ",
+      },
+      {
+        img: diversos_4,
+        nombre: "Control y consola XYZ",
+      },
+      {
+        img: diversos_5,
+        nombre: "Control y consola XYZ",
+      },
+      {
+        img: diversos_6,
+        nombre: "Producto XYZ",
+      },
+    ],
+  },
+];
+
+export const allCategories = {
+  nombre: "Todos los productos",
+  images: [
+    {
+      img: star_1,
+      nombre: "Producto XYZ",
+    },
+    {
+      img: star_2,
+      nombre: "Producto XYZ",
+    },
+    {
+      img: star_3,
+      nombre: "Producto XYZ",
+    },
+    {
+      img: star_4,
+      nombre: "Producto XYZ",
+    },
+    {
+      img: star_5,
+      nombre: "Producto XYZ",
+    },
+    {
+      img: star_6,
+      nombre: "Producto XYZ",
+    },
+    {
+      img: consolas_1,
+      nombre: "Control XYZ",
+    },
+    {
+      img: consolas_2,
+      nombre: "Control y consola XYZ",
+    },
+    {
+      img: consolas_3,
+      nombre: "Consola XYZ",
+    },
+    {
+      img: consolas_4,
+      nombre: "Control XYZ",
+    },
+    {
+      img: consolas_5,
+      nombre: "Consola XYZ",
+    },
+    {
+      img: consolas_6,
+      nombre: "Game Boy Color",
+    },
+    {
+      img: diversos_1,
+      nombre: "Camisa Atari",
+    },
+    {
+      img: diversos_2,
+      nombre: "Camisa SNES",
+    },
+    {
+      img: diversos_3,
+      nombre: "Control y consola XYZ",
+    },
+    {
+      img: diversos_4,
+      nombre: "Control y consola XYZ",
+    },
+    {
+      img: diversos_5,
+      nombre: "Control y consola XYZ",
+    },
+    {
+      img: diversos_6,
+      nombre: "Producto XYZ",
+    },
+  ],
+};
